60cf0c49266e75bc9a681e8192cc8f893eac678b
Travis Ueki <travis.ueki@learningfuze.com>
2016-05-16 11:53:32 -0700

    more practice

---
+++ src/github.com/user/hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"                        // Test comment for go fmt
+	"github.com/user/stringutil" // Second test comment for go fmt
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("\n dlroW oG ,olleH"))
+}
